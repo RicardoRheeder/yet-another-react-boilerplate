f5f7545d5b9623b13cf64c8db76db084e0c97519
kiranbandi <bvenkatkiran@gmail.com>
2018-11-27 18:38:51 -0600

    react

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import TodoList from './TodoList';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      currentText: ''
+    };
+
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+
+  handleInput(event) {
+    this.setState({ currentText: event.target.value });
+  }
+
+  addItem(event) {
+    event.preventDefault();
+    let { currentText, items } = this.state;
+    // Append the items list
+    items.push(currentText);
+    this.setState({
+      items,
+      currentText: ''
+    });
+  }
+
   render() {
     return (
-      <h1>HELLO WORLD - Because anything else is pointless !!!</h1>
+      <div className='App'>
+        <h3>TODO</h3>
+        <TodoList items={this.state.items} />
+        <form onSubmit={this.props.addItem}>
+          <p>What needs to be done ?</p>
+          <input
+            id='add-todo'
+            placeholder="Task"
+            onChange={this.handleInput}
+            value={this.state.currentText} />
+          <button onClick={this.addItem} >Add Task</button>
+        </form>
+      </div>
     )
   }
 }
